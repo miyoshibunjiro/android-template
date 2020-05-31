27c1282f8fc3de88e8a08ebbc036fe34ba4ef45a
RyuNen344 <s1100633@outlook.com>
2020-05-31 00:08:15 +0000

    Upgraded androidx.core:core-ktx from 1.3.0-rc01 to 1.3.0

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -30,7 +30,7 @@ object Dep {
 
     object AndroidX {
 
-        const val coreKtx = "androidx.core:core-ktx:1.3.0-rc01"
+        const val coreKtx = "androidx.core:core-ktx:1.3.0"
         const val appCompat = "androidx.appcompat:appcompat:1.2.0-beta01"
         const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta4"
         const val emoji = "androidx.emoji:emoji-appcompat:1.0.0"
